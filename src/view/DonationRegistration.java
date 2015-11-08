af1b70d421e696e1f37191c77608ae125234770c
joabbremer <joab.bremer@gmail.com>
2015-11-08 11:00:46 -0300

    Tela
    Registro de Doação

---
+++ src/view/DonationRegistration.java
@@ -0,0 +1,167 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JSeparator;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class DonationRegistration extends JInternalFrame {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
+	private JTable table_1;
+
+	
+	public DonationRegistration() {
+		setTitle("Cadastro Doa\u00E7\u00E3o");
+		setClosable(true);
+		setMaximizable(true);
+		setIconifiable(true);
+		setSize(600, 400);
+		
+		JLabel lblDoador = new JLabel("Doador");
+		lblDoador.setForeground(Color.BLUE);
+		lblDoador.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JSeparator separator = new JSeparator();
+		
+		JLabel lblNome = new JLabel("Nome");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		
+		JLabel lblCpf = new JLabel("CPF");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JButton btnNovaDoao = new JButton("Nova Doa\u00E7\u00E3o");
+		
+		JButton btnBuscar = new JButton("Buscar");
+		
+		JLabel lblUltimasDoaes = new JLabel("Ultimas Doa\u00E7\u00F5es");
+		lblUltimasDoaes.setForeground(Color.BLUE);
+		lblUltimasDoaes.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JSeparator separator_2 = new JSeparator();
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JLabel lblStatusDoao = new JLabel("Hist\u00F3rico Doa\u00E7\u00E3o");
+		lblStatusDoao.setForeground(Color.BLUE);
+		lblStatusDoao.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JSeparator separator_3 = new JSeparator();
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblDoador)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(separator, GroupLayout.PREFERRED_SIZE, 172, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(lblStatusDoao)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(separator_3, GroupLayout.PREFERRED_SIZE, 217, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+									.addComponent(lblNome)
+									.addComponent(lblCpf)
+									.addComponent(textField_1)
+									.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.UNRELATED)))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(25)
+									.addComponent(btnNovaDoao)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnBuscar))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblUltimasDoaes)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(separator_2, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE))
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 220, GroupLayout.PREFERRED_SIZE))
+							.addGap(9)
+							.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 322, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(separator_3, GroupLayout.PREFERRED_SIZE, 1, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblStatusDoao)
+						.addComponent(lblDoador)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 318, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNome)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblCpf)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNovaDoao)
+								.addComponent(btnBuscar))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblUltimasDoaes)
+								.addComponent(separator_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(scrollPane, 0, 0, Short.MAX_VALUE))
+						.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 317, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(14, Short.MAX_VALUE))
+		);
+		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Data/Hora", "Status"
+			}
+		));
+		scrollPane_1.setViewportView(table_1);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Data", "Status"
+			}
+		));
+		scrollPane.setViewportView(table);
+		getContentPane().setLayout(groupLayout);
+
+	}
+}
