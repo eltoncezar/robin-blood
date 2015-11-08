e5774b191e4079f072ccf785ecc5951a1ef52f0f
eltoncezar <eltoncezar@gmail.com>
2015-11-08 15:51:35 -0200

    DonorController DE NOVO

--- src/business/DonorController.java
+++ src/business/DonorController.java
@@ -1,5 +1,63 @@
-package business;
+ package business;
+ 
+import java.util.List;
 
-public class DonorController {
+import javax.swing.table.DefaultTableModel;
 
-}
+import models.Donor;
+import data.ConnectException;
+import data.DonorData;
+
+ public class DonorController {
+	private DonorData donorData;
+
+	public DonorController() {
+		donorData = new DonorData();
+	}
+
+	public List<Donor> getAll() {
+		List<Donor> result = null;
+		try {
+			result = donorData.listAll();
+		} catch (ConnectException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public List<Donor> getByFilter(String filter) {
+		List<Donor> result = null;
+		try {
+			result = donorData.selectByName(filter);
+		} catch (ConnectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public DefaultTableModel getTableModel(List<Donor> donors) {
+		DefaultTableModel model = new DefaultTableModel(new String[] { "Id", "Nome", "E-mail" }, 0);
+
+		for (Donor donor : donors) {
+			Object[] o = new Object[3];
+			o[0] = donor.getId();
+			o[1] = donor.getName();
+			o[2] = donor.getEmail();
+
+			model.addRow(o);
+		}
+
+		return model;
+	}
+
+	public Donor save(Donor donor) throws ConnectException {
+		return donorData.save(donor);
+	}
+
+	public void delete(Donor donor) throws ConnectException {
+		donorData.delete(donor);
+	}
+ 
+ }
