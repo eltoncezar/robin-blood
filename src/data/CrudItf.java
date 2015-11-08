5e744961d5a6a6429976551fa3df4a77a5a13674
eltoncezar <eltoncezar@gmail.com>
2015-11-08 11:25:46 -0200

    conexão do banco no Joab

--- src/data/CrudItf.java
+++ src/data/CrudItf.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public interface CrudItf<T> {
 	
-	public final String connection = "jdbc:sqlserver://rdsweb.usb.org.br;databaseName=ROBINBLOOD;user=robin.blood;password=beahero";
+	//public final String connection = "jdbc:sqlserver://rdsweb.usb.org.br;databaseName=ROBINBLOOD;user=robin.blood;password=beahero";
+	public final String connection = "jdbc:sqlserver://192.168.0.4;databaseName=ROBINBLOOD;user=sa;password=123456";
 	
 	public List<T> listAll() throws ConnectException;
 	public T select(int id) throws ConnectException;
