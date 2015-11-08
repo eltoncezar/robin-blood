8f5ffee9f2514e3c900fb5404efea8b455196762
joabbremer <joab.bremer@gmail.com>
2015-11-08 14:36:34 -0300

    reiniciar Frame

--- src/view/UserList.java
+++ src/view/UserList.java
@@ -1,28 +1,22 @@
 package view;
 
-import java.awt.Component;
-import java.awt.EventQueue;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JInternalFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
 
-import data.ConnectException;
-import models.User;
 import business.UserRegistrationController;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import data.ConnectException;
+
+import javax.swing.JFrame;
 
 public class UserList extends JInternalFrame {
 	private JTextField textField;
@@ -30,9 +24,8 @@ public class UserList extends JInternalFrame {
 	private UserRegistrationController controller;
 
 	public UserList() {
-		controller = new UserRegistrationController();
-
 		setClosable(true);
+		controller = new UserRegistrationController();
 		setTitle("Listar Usu\u00E1rios");
 		// setBounds(100, 100, 450, 300);
 		setSize(450, 350);
@@ -56,67 +49,38 @@ public class UserList extends JInternalFrame {
 
 		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout
-				.setHorizontalGroup(groupLayout
-						.createParallelGroup(Alignment.LEADING)
-						.addGroup(
-								groupLayout
-										.createSequentialGroup()
-										.addGap(18)
-										.addGroup(
-												groupLayout
-														.createParallelGroup(
-																Alignment.TRAILING)
-														.addComponent(
-																scrollPane,
-																GroupLayout.DEFAULT_SIZE,
-																406,
-																Short.MAX_VALUE)
-														.addGroup(
-																groupLayout
-																		.createSequentialGroup()
-																		.addComponent(
-																				textField,
-																				GroupLayout.DEFAULT_SIZE,
-																				323,
-																				Short.MAX_VALUE)
-																		.addGap(18)
-																		.addComponent(
-																				btnBuscar))
-														.addComponent(lblNome))
-										.addContainerGap())
-						.addGroup(
-								Alignment.TRAILING,
-								groupLayout.createSequentialGroup()
-										.addContainerGap(341, Short.MAX_VALUE)
-										.addComponent(btnEditar).addGap(32)));
-		groupLayout
-				.setVerticalGroup(groupLayout
-						.createParallelGroup(Alignment.LEADING)
-						.addGroup(
-								groupLayout
-										.createSequentialGroup()
-										.addContainerGap()
-										.addComponent(lblNome)
-										.addPreferredGap(
-												ComponentPlacement.RELATED)
-										.addGroup(
-												groupLayout
-														.createParallelGroup(
-																Alignment.BASELINE)
-														.addComponent(
-																textField,
-																GroupLayout.PREFERRED_SIZE,
-																GroupLayout.DEFAULT_SIZE,
-																GroupLayout.PREFERRED_SIZE)
-														.addComponent(btnBuscar))
-										.addPreferredGap(
-												ComponentPlacement.RELATED)
-										.addComponent(scrollPane,
-												GroupLayout.PREFERRED_SIZE,
-												201, GroupLayout.PREFERRED_SIZE)
-										.addGap(18).addComponent(btnEditar)
-										.addContainerGap(19, Short.MAX_VALUE)));
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(textField, GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)
+							.addGap(18)
+							.addComponent(btnBuscar))
+						.addComponent(lblNome))
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(341, Short.MAX_VALUE)
+					.addComponent(btnEditar)
+					.addGap(32))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNome)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnBuscar))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnEditar)
+					.addContainerGap(19, Short.MAX_VALUE))
+		);
 
 		scrollPane.setViewportView(table);
 		getContentPane().setLayout(groupLayout);
